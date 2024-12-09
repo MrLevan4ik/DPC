81a40e6e6271b0aa36b3c0d5d57ba29491e543d7
LEV <144740395+MrLevan4ik@users.noreply.github.com>
2024-12-09 14:51:33 +0300

    Update fuction.c

--- fuction.c
+++ fuction.c
@@ -4,16 +4,33 @@
 #include <time.h>
 #include "function.h"
 #include <string.h>
+#include <dirent.h>
+#include <unistd.h> // Для chdir и getcwd
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdbool.h>
 
-char programVersion[] = "0.1";
+#define MAX_LINES 100
+#define LINE_LENGTH 256
+
+char programVersion[] = "0.2";
 
 // Список команд
 Command commands[] = {
     {"help", "Выводит список команд", help_command},
+    {"hello", "Приветствие", greeting_command},
     {"print", "Выводит текст после команды", print_command},
     {"time", "Выводит время в формате часы:минуты:секунды", time_command},
     {"date", "Выводит дату в формате дни/месяцы/годы", date_command},
-    {"V", "Выводит версию программы", version_command}
+    {"v", "Выводит версию программы", version_command},
+    {"clear", "Очищает консоль", clear_command},
+    {"cd", "Переходит в указанную директорию", cd_command},
+    {"mkdir", "Создает новую папку", mkdir_command},
+    {"create", "Создает новый файл", create_file_command},
+    {"rm", "Удаляет файл или папку", rm_command},
+    {"ls", "Отображает список файлов и папок в текущей директории", ls_command},
+    {"read", "Читает содержимое файла", read_file_command},
+    {"pwd", "Отображает текущий путь", pwd_command}
 };
 
 // Функция-обработчик для команды "print"
@@ -65,6 +82,123 @@ void date_command(char *arg[]) {
 void version_command(char *arg[]) {
     if (arg == NULL || arg[0] == NULL) {
         printf("%s\n", programVersion);    
+    } else {
+        printf("Неверный ввод\n");
+    }
+}
+
+// Очистка консоли
+void clear_command(char *arg[]) {
+    system("clear || cls");
+}
+
+// Перемещение по директории
+void cd_command(char *arg[]) {
+    if (arg != NULL && arg[0] != NULL) {
+        if (chdir(arg[0]) == 0) {
+            printf("Перешли в директорию: %s\n", arg[0]);
+        } else {
+            perror("Ошибка при переходе");
+        }
+    } else {
+        printf("Не указана директория.\n");
+    }
+}
+
+// Создание папки
+void mkdir_command(char *arg[]) {
+    if (arg != NULL && arg[0] != NULL) {
+        if (mkdir(arg[0], 0777) == 0) {
+            printf("Папка %s создана.\n", arg[0]);
+        } else {
+            perror("Ошибка при создании папки");
+        }
+    } else {
+        printf("Не указано имя папки.\n");
+    }
+}
+
+// Создание файла
+void create_file_command(char *arg[]) {
+    if (arg != NULL && arg[0] != NULL) {
+        FILE *file = fopen(arg[0], "w");
+        if (file) {
+            printf("Файл %s создан.\n", arg[0]);
+            fclose(file);
+        } else {
+            perror("Ошибка при создании файла");
+        }
+    } else {
+        printf("Не указано имя файла.\n");
+    }
+}
+
+// Удаление папки/файла
+void rm_command(char *arg[]) {
+    if (arg != NULL && arg[0] != NULL) {
+        if (remove(arg[0]) == 0) {
+            printf("Файл или папка %s удалены.\n", arg[0]);
+        } else {
+            perror("Ошибка при удалении");
+        }
+    } else {
+        printf("Не указано имя файла или папки.\n");
+    }
+}
+
+// Отображение файлов в текущей директории
+void ls_command(char *arg[]) {
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(".");
+    if (dir) {
+        printf("Содержимое текущей директории:\n");
+        while ((entry = readdir(dir)) != NULL) {
+            printf("- %s\n", entry->d_name);
+        }
+        closedir(dir);
+    } else {
+        perror("Ошибка при открытии директории");
+    }
+}
+
+// Чтение файла
+void read_file_command(char *arg[]) {
+    if (arg != NULL && arg[0] != NULL) {
+        FILE *file = fopen(arg[0], "r");
+        if (file) {
+            char line[256];
+            printf("Содержимое файла %s:\n", arg[0]);
+            while (fgets(line, sizeof(line), file)) {
+                printf("%s", line);
+            }
+            fclose(file);
+        } else {
+            perror("Ошибка при открытии файла");
+        }
+    } else {
+        printf("Не указано имя файла.\n");
+    }
+}
+
+// Показать текущий путь
+void pwd_command(char *arg[]) {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd))) {
+        printf("Текущая директория: %s\n", cwd);
+    } else {
+        perror("Ошибка получения текущей директории");
+    }
+}
+
+// Приветствие
+void greeting_command(char *arg[]) {
+    if (arg == NULL || arg[0] == NULL) {
+        printf("Привет, это DPC(Digit Personal Console).\n");
+        printf("Созданная в обучающих целях и является\n"); 
+        printf("самой базовой и премитивной моделью консоли.\n");
+        printf("Это версия: %s. Введи help чтобы получить список доступных команд\n", programVersion);    
     } else {
         printf("Неверный ввод\n");
     }
