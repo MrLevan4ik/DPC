04129906654dbac3c28a5ce0ee695083ff6a9699
LEV <144740395+MrLevan4ik@users.noreply.github.com>
2024-12-09 14:51:16 +0300

    Update function.h

--- function.h
+++ function.h
@@ -14,6 +14,16 @@ void help_command(char *arg[]);
 void time_command(char *arg[]);
 void date_command(char *arg[]);
 void version_command(char *arg[]);
+void clear_command(char *arg[]);
+void cd_command(char *arg[]);
+void mkdir_command(char *arg[]);
+void create_file_command(char *arg[]);
+void rm_command(char *arg[]);
+void ls_command(char *arg[]);
+void read_file_command(char *arg[]);
+void pwd_command(char *arg[]);
+void greeting_command(char *arg[]);
+
 void execute_command(char *first_word, char **args);
 
 #endif
